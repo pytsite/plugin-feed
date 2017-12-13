2bdfb64761fee39e003aad4b7fac218c5699c476
Alexander Shepetko <a@shepetko.com>
2017-12-13 17:07:57 +0200

    Release 1.1

--- __init__.py
+++ __init__.py
@@ -1,8 +1,11 @@
 """PytSite RSS/Atom Reader and Writer.
 """
-# Public API
-from . import _abstract as abstract, _xml as xml, _rss as rss
-
 __author__ = 'Alexander Shepetko'
 __email__ = 'a@shepetko.com'
 __license__ = 'MIT'
+
+from pytsite import plugman as _plugman
+
+if _plugman.is_installed(__name__):
+    # Public API
+    from . import _abstract as abstract, _xml as xml, _rss as rss
